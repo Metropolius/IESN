795e695c80664e49e05589d56c585802351f2de6
LABAISSE Simon <etu27985@henallux.be>
2016-09-29 15:25:57 +0200

    hello

---
+++ Labo2/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.Entity;
+using Labo2;
+using System.Linq;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1 
+    {
+        [TestInitialize]
+        public void InsertionFonctionnelle()
+        {
+            Database.SetInitializer(new DbInitializer());
+            using (CompanyContext context = GetContext())
+            {
+                context.Database.Initialize(true);
+            }
+        }
+
+        [TestMethod]
+        public void CanGetCustomers()
+        {
+            using (var context = GetContext())
+            {
+                Assert.AreEqual(1, context.Customers.ToList().Count);
+            }
+        }
+
+        public CompanyContext GetContext()
+        {
+            CompanyContext context = new CompanyContext();
+            return context;
+        }
+    }
+}
